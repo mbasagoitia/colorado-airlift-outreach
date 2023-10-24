f7edf5f7b1ddb7829abafc017c77b1b9199912be
basagoitiam <marika.basagoitia@gmail.com>
2023-10-24 16:22:11 -0400

    added alt tags to homepage image slider images

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -4,25 +4,25 @@ function ImageSlider () {
   return (
     <Carousel>
       <Carousel.Item interval={4000}>
-        <img src="/images/home/hp-image-1.jpg" alt="" />
+        <img src="/images/home/hp-image-1.jpg" alt="cliffs" />
       </Carousel.Item>
       <Carousel.Item interval={4000}>
-        <img src="/images/home/hp-image-2.jpg" alt="" />
+        <img src="/images/home/hp-image-2.jpg" alt="airplane" />
       </Carousel.Item>
       <Carousel.Item interval={4000}>
-        <img src="/images/home/hp-image-3.jpg" alt="" />
+        <img src="/images/home/hp-image-3.jpg" alt="people sitting outside" />
       </Carousel.Item>
       <Carousel.Item interval={4000}>
-        <img src="/images/home/hp-image-4.jpg" alt="" />
+        <img src="/images/home/hp-image-4.jpg" alt="children" />
       </Carousel.Item>
       <Carousel.Item interval={4000}>
-        <img src="/images/home/hp-image-5.jpg" alt="" />
+        <img src="/images/home/hp-image-5.jpg" alt="group of volunteers" />
       </Carousel.Item>
       <Carousel.Item interval={4000}>
-        <img src="/images/home/hp-image-6.jpg" alt="" />
+        <img src="/images/home/hp-image-6.jpg" alt="optometrists working with a patient" />
       </Carousel.Item>
       <Carousel.Item interval={4000}>
-        <img src="/images/home/hp-image-7.jpg" alt="" />
+        <img src="/images/home/hp-image-7.jpg" alt="CALO team standing in front of airplane" />
       </Carousel.Item>
     </Carousel>
   );
