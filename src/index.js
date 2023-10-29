5c5ff9fa5c56a54cc4bcd13b89516fd7d7ef3794
basagoitiam <marika.basagoitia@gmail.com>
2023-10-29 17:20:08 -0400

    troubleshooting yaml

--- src/index.js
+++ src/index.js
@@ -10,104 +10,16 @@ import netlifyIdentity from "netlify-identity-widget";
 import jsyaml from "js-yaml";
 
 const yamlContent = `
-media_folder: 'public/images/uploads'
-public_folder: '/images/uploads'
-collections:
-  name: 'home'
-  label: 'Home'
-  folder: 'content/pages/home'
+backend:
+  name: test-repo
+
+collections: 
+- name: 'posts'
+  label: 'Posts'
+  folder: 'content/posts'
   create: true
   fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'mission'
-label: 'Mission'
-folder: 'content/pages/mission'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'what-we-do'
-label: 'What We Do'
-folder: 'content/pages/what-we-do'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'clinic-schedule'
-label: 'Clinic Schedule'
-folder: 'content/pages/clinic-schedule'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'clinic-trip-checklist'
-label: 'Clinic Trip Checklist'
-folder: 'content/pages/resources/clinic-trip-checklist'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'emergency-contacts'
-label: 'Emergency Contacts'
-folder: 'content/pages/resources/emergency-contacts'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'links'
-label: 'Links'
-folder: 'content/pages/resources/links'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'pilot-information'
-label: 'Pilot Information'
-folder: 'content/pages/pilot-information'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'contact'
-label: 'Contact'
-folder: 'content/pages/contact'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-roles: ["admin"]
-- name: 'videos-photos-collection1'
-label: 'Videos and Photos Collection 1'
-folder: 'content/pages/resources/videos-and-photos/collection1'
-create: true
-fields:
-  { name: 'title', label: 'Title', widget: 'string' }
-  { name: 'body', label: 'Body', widget: 'markdown' }
-  { name: 'images', label: 'Images', widget: 'list', fields: [
-    { name: 'image', label: 'Image', widget: 'image' }
-  ]
-}
-roles: ["admin"]
-site_config:
-name: 'site-config'
-file: 'data/site-config.json'
-label: 'Site Configuration'
-create: true
-delete: true
-editor:
-  preview: true
-fields:
-  - { name: 'title', label: 'Site Title', widget: 'string' }
-  - { name: 'metaDescription', label: 'Meta Description', widget: 'text' }
+    - { label: "Title", name: 'title', widget: 'string' }
 `
 const parsedYamlContent = jsyaml.load(yamlContent);
 
