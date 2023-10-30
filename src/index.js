7af3c9ee25cb09d36f484e31d32d0fd4b9603595
basagoitiam <marika.basagoitia@gmail.com>
2023-10-30 11:35:50 -0400

    removed netlify cms import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CMS from "netlify-cms";
 import netlifyIdentity from "netlify-identity-widget";
 import jsyaml from "js-yaml";
 
