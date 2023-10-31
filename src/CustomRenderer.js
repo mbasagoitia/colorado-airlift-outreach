bf761e085b321bba3973cbd4ccfdb444f70642bf
basagoitiam <marika.basagoitia@gmail.com>
2023-10-31 09:48:53 -0400

    edited renderer function to render images

--- src/CustomRenderer.js
+++ src/CustomRenderer.js
@@ -2,39 +2,46 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 
 function MyCustomRenderer(props) {
-        const { content } = props;
-
-        const lines = content.split('\n');
-
-        let isWithinParagraph = false;
-        let output = [];
-
-        let skipFrontMatter = false;
-
-        for (let line of lines) {
-            if (line.trim() === '---') {
-                skipFrontMatter = !skipFrontMatter;
-            } else if (!skipFrontMatter) {
-                const parts = line.split('{className="');
-            if (parts.length > 1) {
-                const [className, text] = parts[1].split('"}');
-                output.push(
-                        <ReactMarkdown key={output.length} className={className} children={text} />
-                );
-                isWithinParagraph = false;
-            } else {
-                if (!isWithinParagraph) {
-                    output.push(<ReactMarkdown key={output.length} children={line} />);
-                }
+    const { content } = props;
+  
+    const lines = content.split('\n');
+  
+    let isWithinParagraph = false;
+    let output = [];
+    let skipFrontMatter = false;
+  
+    for (let line of lines) {
+      if (line.trim() === '---') {
+        skipFrontMatter = !skipFrontMatter;
+      } else if (!skipFrontMatter) {
+        if (line.match(/\!\[.*\]\(.*\)/)) {
+          // Check for lines containing image references
+          const imagePath = line.match(/\!\[.*\]\(([^)]+)\)/)[1];
+          output.push(<img key={output.length} src={imagePath} alt="Image" />);
+          isWithinParagraph = false;
+        } else {
+          const parts = line.split('{className="');
+          if (parts.length > 1) {
+            const [className, text] = parts[1].split('"}');
+            output.push(
+              <ReactMarkdown key={output.length} className={className} children={text} />
+            );
+            isWithinParagraph = false;
+          } else {
+            if (!isWithinParagraph) {
+              output.push(<ReactMarkdown key={output.length} children={line} />);
             }
-            }
+          }
         }
-
-        return (
-            <div className="markdown-content">
-                {output}
-            </div>
-        )
-}
+      }
+    }
+  
+    return (
+      <div className="markdown-content">
+        {output}
+      </div>
+    );
+  }
+  
 
 export default MyCustomRenderer;
