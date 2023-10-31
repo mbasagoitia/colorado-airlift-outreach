39f3495d85f19d79c9cee3f40d1f29bd6adbb946
basagoitiam <marika.basagoitia@gmail.com>
2023-10-31 10:07:08 -0400

    edited renderer function to include classNames on images

--- src/CustomRenderer.js
+++ src/CustomRenderer.js
@@ -3,48 +3,42 @@ import ReactMarkdown from "react-markdown";
 
 function MyCustomRenderer(props) {
     const { content } = props;
-  
+
     const lines = content.split('\n');
-  
     let isWithinParagraph = false;
-    let output = [];
-    let skipFrontMatter = false;
-  
+    const output = [];
+
     for (let line of lines) {
-      if (line.trim() === '---') {
-        skipFrontMatter = !skipFrontMatter;
-      } else if (!skipFrontMatter) {
-        if (line.match(/\!\[.*\]\(.*\)/)) {
-          // Check for lines containing image references
-          const match = line.match(/\!\[.*\]\(([^)]+)\)/);
-        if (match) {
-        const imagePath = match[1]; // This captures only the image path
-        output.push(<img key={output.length} src={imagePath} alt="Image" />);
-        }
-          isWithinParagraph = false;
-        } else {
-          const parts = line.split('{className="');
-          if (parts.length > 1) {
+        const parts = line.split('{className="');
+        if (parts.length > 1) {
             const [className, text] = parts[1].split('"}');
-            output.push(
-              <ReactMarkdown key={output.length} className={className} children={text} />
-            );
-            isWithinParagraph = false;
-          } else {
-            if (!isWithinParagraph) {
-              output.push(<ReactMarkdown key={output.length} children={line} />);
+            if (line.includes('![')) {
+                // Handle images
+                const imagePath = line.match(/\!\[.*\]\(([^)]+)\)/)[1];
+                output.push(
+                    <img key={output.length} src={imagePath} alt="Image" className={className} />
+                );
+            } else {
+                // Handle other content
+                output.push(
+                    <ReactMarkdown key={output.length} className={className} children={text} />
+                );
+                isWithinParagraph = false;
             }
-          }
+        } else {
+            if (!isWithinParagraph) {
+                output.push(<ReactMarkdown key={output.length} children={line} />);
+            }
         }
-      }
     }
-  
+
     return (
-      <div className="markdown-content">
-        {output}
-      </div>
+        <div className="markdown-content">
+            {output}
+        </div>
     );
-  }
+}
+
   
 
 export default MyCustomRenderer;
