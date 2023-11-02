ef427671715fc5bb652e4d4d9ecab98f7f22a2e6
basagoitiam <marika.basagoitia@gmail.com>
2023-11-01 21:32:53 -0400

    removed fetch request from collection1 page

--- src/pages/resources/videos-photos/collection1.jsx
+++ src/pages/resources/videos-photos/collection1.jsx
@@ -1,29 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-function PhotoCollection1() {
-  const [userImages, setUserImages] = useState([]);
-  
+function PhotoCollection1(props) {
+  const { title, images } = props;
+
+  // Default images
   const defaultImages = [
     { path: '/path/to/default-image-1.jpg', alt: 'Default Image 1' },
     { path: '/path/to/default-image-2.jpg', alt: 'Default Image 2' },
   ];
 
-  // Fetch user-uploaded images from your custom endpoint
-  useEffect(() => {
-    fetch('https://harmonious-toffee-b74135.netlify.app/images')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setUserImages(data.userImages); // Assuming your data structure includes userImages
-      });
-  }, []);
-
   // Combine the default images with user-uploaded images
-  const allImages = [...defaultImages, ...userImages];
+  const allImages = [...defaultImages, ...images];
 
   return (
     <div>
-      <h1>Photo Collection</h1>
+      <h1>{title}</h1>
       <div className="image-container">
         {allImages.map((image, index) => (
           <img key={index} src={image.path} alt={image.alt} />
@@ -34,4 +26,3 @@ function PhotoCollection1() {
 }
 
 export default PhotoCollection1;
-
