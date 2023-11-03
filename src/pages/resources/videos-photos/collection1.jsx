b79bc0e487de927cfcb53dc2650d95e04055c763
basagoitiam <marika.basagoitia@gmail.com>
2023-11-03 13:49:49 -0400

    another log

--- src/pages/resources/videos-photos/collection1.jsx
+++ src/pages/resources/videos-photos/collection1.jsx
@@ -21,6 +21,7 @@ function PhotoCollection1() {
         })
         .then((data) => {
             console.log(data);
+            console.log(grayMatter(data));
             setMarkdownContent(grayMatter(data));
         })
         .catch((err) => {
