913f9e03a1e2350b4a06370918f418b3b7362bc0
basagoitiam <marika.basagoitia@gmail.com>
2023-11-06 10:51:51 -0500

    added netlify identity open

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import netlifyIdentity from "netlify-identity-widget";
+import netlifyIdentity from 'netlify-identity-widget';
 
-netlifyIdentity.init();
+netlifyIdentity.init({
+  container: '#netlify-modal', // A DOM element to render the modal dialog
+});
+
+netlifyIdentity.on('login', (user) => {
+  // Do something when the user logs in
+});
+
+netlifyIdentity.on('logout', () => {
+  // Do something when the user logs out
+});
+netlifyIdentity.open();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
