bbd3217a2f0893744a5c9d9c5185e529b9188e90
basagoitiam <marika.basagoitia@gmail.com>
2023-11-06 10:03:55 -0500

    added updated files since reset

---
+++ src/components/PhotoGrid.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+function PhotoGrid({ images }) {
+  
+  const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
+  const [lightboxIndex, setLightboxIndex] = useState(0);
+
+  const openLightbox = (index) => {
+    setLightboxIsOpen(true);
+    setLightboxIndex(index);
+  };
+
+  const closeLightbox = () => {
+    setLightboxIsOpen(false);
+  };
+
+  const prevImage = () => {
+    setLightboxIndex((lightboxIndex + images.length - 1) % images.length);
+  };
+
+  const nextImage = () => {
+    setLightboxIndex((lightboxIndex + 1) % images.length);
+  };
+
+  return (
+    <div className="photo-grid">
+      {images.map((image, index) => (
+        <div
+          className="photo-grid-item"
+          key={index}
+          onClick={() => openLightbox(index)}
+        >
+          <img src={image} alt={`Image ${index}`} />
+        </div>
+      ))}
+
+      {lightboxIsOpen && (
+        <div className="lightbox">
+          <span className="close" onClick={closeLightbox}>
+            &times;
+          </span>
+          <img src={images[lightboxIndex]} alt={`Image ${images[lightboxIndex]}`} />
+          <a className="prev" onClick={prevImage}>
+            &#8249;
+          </a>
+          <a className="next" onClick={nextImage}>
+            &#8250;
+          </a>
+          <div className="progress-bar">
+            {images.map((_, index) => (
+              <span
+                key={index}
+                className={`progress-dot ${index === lightboxIndex ? 'active' : ''}`}
+                onClick={() => setLightboxIndex(index)}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PhotoGrid;
+
--- src/pages/resources/videos-photos/collection1.jsx
+++ src/pages/resources/videos-photos/collection1.jsx
@@ -1,9 +1,10 @@
+import React, { useState, useEffect } from "react";
+import frontMatter from "front-matter";
 import { Container } from "react-bootstrap";
 import Header from "../../../components/Header";
 import Navigation from "../../../components/Navigation";
 import Footer from "../../../components/Footer";
-import React, { useState, useEffect } from "react";
-import frontMatter from "front-matter";
+import PhotoGrid from "../../../components/PhotoGrid";
 
 function PhotoCollection1() {
     
@@ -34,22 +35,21 @@ function PhotoCollection1() {
 
     const allImages = [...defaultImages, ...imagePaths];
 
-  return (
-    <Container fluid>
-        <Header />
+    return (
+        <Container fluid>
+            <Header />
             <Navigation />
             <div className="full-page-img">
                 <img src="/images/resources/emergency-contacts-header.jpg" alt="CALO team" className="header-img" />
             </div>
             <div className="page-content">
-                <div className="image-container">
-                    {/* Default images */}
-                {allImages.map((imagePath, index) => <img key={index} src={imagePath} alt={`Image ${index + 1}`} />)}
-                </div>
+                <h1>Collection 1</h1>
+                <p className="text-muted">Click on any photo to enlarge</p>
+                <PhotoGrid images={allImages} />
             </div>
-        <Footer />
-    </Container>
-  );
+            <Footer />
+        </Container>
+    );
 }
 
 export default PhotoCollection1;
