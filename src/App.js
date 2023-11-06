fe987085d9974833973800a6e49fb1fabf973f85
basagoitiam <marika.basagoitia@gmail.com>
2023-11-06 13:24:19 -0500

    removed routes that don't exist yet

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ClinicSchedule from './pages/ClinicSchedule';
 import Checklist from './pages/resources/Checklist';
 import EmergencyContacts from './pages/resources/EmergencyContacts';
 import VideosAndPhotos from './pages/resources/VideosAndPhotos';
-import PhotoCollection1 from './components/PhotoCollection';
+import Collection1 from './pages/resources/videos-photos/collection1';
 import Links from './pages/resources/Links';
 import PilotInformation from './pages/PilotInformation';
 import Contact from './pages/Contact';
@@ -24,13 +24,13 @@ function App() {
         <Route path="resources/emergency-contacts" element={<EmergencyContacts />} />
         <Route path="resources/videos-and-photos" element={<VideosAndPhotos />} />
         <Route path="resources/videos-and-photos/team-of-airplanes" element={<Collection1 />} />
-        <Route path="resources/videos-and-photos/colorado-team" element={<Collection2 />} />
+        {/* <Route path="resources/videos-and-photos/colorado-team" element={<Collection2 />} />
         <Route path="resources/videos-and-photos/flying-to-mexico" element={<Collection3 />} />
         <Route path="resources/videos-and-photos/patients-waiting-for-eye-clinic" element={<Collection4 />} />
         <Route path="resources/videos-and-photos/optometry-clinic" element={<Collection5 />} />
         <Route path="resources/videos-and-photos/eye-surgery-clinic" element={<Collection6 />} />
         <Route path="resources/videos-and-photos/spiritual-ministry" element={<Collection7 />} />
-        <Route path="resources/videos-and-photos/celebrating-a-successful-clinic" element={<Collection8 />} />
+        <Route path="resources/videos-and-photos/celebrating-a-successful-clinic" element={<Collection8 />} /> */}
         <Route path="resources/links" element={<Links />} />
         <Route path="pilot-information" element={<PilotInformation />} />
         <Route path="contact" element={<Contact />} />
