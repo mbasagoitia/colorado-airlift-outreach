ab1fd173d1d918baf4247c456cc282def36f451d
basagoitiam <marika.basagoitia@gmail.com>
2023-11-06 10:31:54 -0500

    added netlify identity widget to index.html

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import netlifyIdentity from "netlify-identity-widget";
-import jsyaml from "js-yaml";
-
-netlifyIdentity.init();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
