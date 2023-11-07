e95459b9c6d0aea590a800061c5b76e47f3323d1
basagoitiam <marika.basagoitia@gmail.com>
2023-11-07 13:17:11 -0500

    added left property for second photo preview

--- src/components/PhotoPreviews.jsx
+++ src/components/PhotoPreviews.jsx
@@ -1,10 +1,17 @@
-function PhotoPreviews({ photo1, photo2 }) {
-    return (
-        <>
-        <img src={photo1} alt="small preview" className="photo-preview photo-preview-1" />
-        <img src={photo2} alt="small preview" className="photo-preview photo-preview-2" />
-        </>
-    )
+import React from 'react';
+
+function PhotoPreviews({ title, photo1, photo2 }) {
+  return (
+    <div className="photo-previews-container">
+      <div className="photo-preview photo-preview-1">
+        <img src={photo1} alt="Small Preview 1" />
+      </div>
+      <div className="photo-preview photo-preview-2">
+        <img src={photo2} alt="Small Preview 2" />
+      </div>
+      <div className="title">{title}</div>
+    </div>
+  );
 }
 
 export default PhotoPreviews;
