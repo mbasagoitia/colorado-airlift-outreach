ede4ea4235e70bc2022d85a0f658348ac2586083
basagoitiam <marika.basagoitia@gmail.com>
2023-11-07 11:56:57 -0500

    changed all video types to video/mp4

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -36,7 +36,7 @@ function PhotoGrid({ images }) {
             onClick={() => openLightbox(index)}
           >
             <video controls>
-              <source src={image} type={`video/${fileExtension}`} />
+              <source src={image} type={`video/mp4`} />
               Your browser does not support the video tag.
             </video>
           </div>
