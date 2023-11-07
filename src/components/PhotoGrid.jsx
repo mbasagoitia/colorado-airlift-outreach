d4f31fd6a12249981054241fc49ae91447f01059
basagoitiam <marika.basagoitia@gmail.com>
2023-11-07 12:25:44 -0500

    changed lightbox to render videos as well as photos

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function PhotoGrid({ images }) {
-  
+
   const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
   const [lightboxIndex, setLightboxIndex] = useState(0);
 
@@ -25,60 +25,66 @@ function PhotoGrid({ images }) {
   return (
     <div className="photo-grid">
       {images.map((image, index) => {
-      const fileExtension = image.split('.').pop().toLowerCase();
+        const fileExtension = image.split('.').pop().toLowerCase();
 
-      if (['mp4'].includes(fileExtension)) {
-        return (
-          <div
-            className="photo-grid-item"
-            key={index}
-            onClick={() => openLightbox(index)}
-          >
-            <video controls>
-              <source src={image} type={"video/mp4"} />
-              Your browser does not support the video tag.
-            </video>
-          </div>
-        );
-      } else {
-        return (
-          <div
-            className="photo-grid-item"
-            key={index}
-            onClick={() => openLightbox(index)}
-          >
-            <img src={image} alt={`Image ${index}`} />
-          </div>
-        );
-      }
-    })}
-
-      {lightboxIsOpen && (
-        <div className="lightbox">
-          <span className="close" onClick={closeLightbox}>
-            &times;
-          </span>
+        if (['mp4'].includes(fileExtension)) {
+          return (
+            <div
+              className="photo-grid-item"
+              key={index}
+              onClick={() => openLightbox(index)}
+            >
+              <video controls>
+                <source src={image} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          );
+        } else {
+          return (
+            <div
+              className="photo-grid-item"
+              key={index}
+              onClick={() => openLightbox(index)}
+            >
+              <img src={image} alt={`Image ${index}`} />
+            </div>
+          );
+        }
+      })}
+      
+    {lightboxIsOpen && (
+      <div className="lightbox">
+        <span className="close" onClick={closeLightbox}>
+          &times;
+        </span>
+        {images[lightboxIndex].toLowerCase().endsWith('.mp4') ? (
+          <video controls autoPlay>
+            <source src={images[lightboxIndex]} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
           <img src={images[lightboxIndex]} alt={`Image ${images[lightboxIndex]}`} />
-          <a className="prev" onClick={prevImage}>
-            &#8249;
-          </a>
-          <a className="next" onClick={nextImage}>
-            &#8250;
-          </a>
-          <div className="progress-bar">
-            {images.map((_, index) => (
-              <span
-                key={index}
-                className={`progress-dot ${index === lightboxIndex ? 'active' : ''}`}
-                onClick={() => setLightboxIndex(index)}
-              />
-            ))}
-          </div>
+        )}
+        <a className="prev" onClick={prevImage}>
+          &#8249;
+        </a>
+        <a className="next" onClick={nextImage}>
+          &#8250;
+        </a>
+        <div className="progress-bar">
+          {images.map((_, index) => (
+            <span
+              key={index}
+              className={`progress-dot ${index === lightboxIndex ? 'active' : ''}`}
+              onClick={() => setLightboxIndex(index)}
+            />
+          ))}
         </div>
-      )}
-    </div>
-  );
+      </div>
+    )}
+  </div>
+);
 }
 
 export default PhotoGrid;
-
